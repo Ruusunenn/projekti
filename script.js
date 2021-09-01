e545a88119f5c5bbbef2f0ae603b22dedf1791e6
Ruusunenn <88773191+Ruusunenn@users.noreply.github.com>
2021-09-01 11:24:59 +0000

    emtt

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+document.querySelector('#lisaanappi').addEventListener('click', e=>{
+  e.preventDefault();
+  if (document.querySelector('#nimi').value == '' || document.querySelector('#tieto').value == ''){
+    document.querySelector('#ilmoitus').innerHTML ='täytä kentät';
+    setTimeout(function(){ document.querySelector('#ilmoitus').innerHTML =''; }, 3000);
+  }
+  else{
+    document.querySelector('#ilmoitus').innerHTML =document.querySelector('#nimi').value + ': ' +document.querySelector('#tieto').value;
+  }
+  });
+
+
 
 
 document.querySelector('.aasi').addEventListener('click', e=>{
@@ -5,23 +17,42 @@ document.querySelector('.aasi').addEventListener('click', e=>{
   console.log("nappi toimii");
   
 if (document.querySelector('#teht_a').checked){
-    document.querySelector('.boksit').children[0].innerHTML = '<li class="sisalto"><input type="checkbox" id="teht_a" checked> toimi </li>' ;
+    document.querySelector('.boksit').children[0].innerHTML = '<input type="checkbox" id="teht_a" checked> toimi ' ;
     
 }
 
   else{
-    document.querySelector('.boksit').children[0].innerHTML = '<li class="sisalto"><input type=checkbox" id="teht_a" >html</li>' ;
+    document.querySelector('.boksit').children[0].innerHTML = '<input type="checkbox" id="teht_a" >html' ;
 }
 
 
 if (document.querySelector('#teht_b').checked){
-    document.querySelector('.boksit').children[0].innerHTML = '<li class="sisalto"><input type="checkbox" id="teht_b" checked> toimi </li>' ;
+    document.querySelector('.boksit').children[1].innerHTML = '<input type="checkbox" id="teht_b" checked> toimi' ;
     
 }
 
 
   else{
-    document.querySelector('.boksit').children[0].innerHTML = '<li class="sisalto"><input type=checkbox" id="teht_b" >html</li>' ;
+    document.querySelector('.boksit').children[1].innerHTML = '<input type="checkbox" id="teht_b" >css' ;
 }
+if (document.querySelector('#teht_c').checked){
+    document.querySelector('.boksit').children[2].innerHTML = '<input type="checkbox" id="teht_c" checked> toimi' ;
+    
+}
+
+
+  else{
+    document.querySelector('.boksit').children[2].innerHTML = '<input type="checkbox" id="teht_c" >JAVASCRIPT' ;
+}
+if (document.querySelector('#teht_d').checked){
+    document.querySelector('.boksit').children[3].innerHTML = '<input type="checkbox" id="teht_d" checked> toimi' ;
+    
+}
+
+
+  else{
+    document.querySelector('.boksit').children[3].innerHTML = '<input type="checkbox" id="teht_d" >CI/CD' ;
+}
+
 
 });
